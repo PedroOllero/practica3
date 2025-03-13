933c2f47473885a6947d7b04f36f93fd18159c51
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-13 12:56:37 +0100

    add Grupo interface and sample music groups to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,48 @@
-import "./style.css";
+interface Grupo {
+    nombre: string,
+    año: number,
+    activo: boolean,
+    genero: string
+}
 
-console.log("Hello Typescript!");
+const PopRock: string = '🎵Pop Rock'
+const Rock: string = '🎸Rock'
+const hardRock: string = '🤘Hard Rock'
+const clasica: string = '🎼Clásica'
+
+const Grupo1: Grupo = {
+    nombre: 'The Beatles',
+    año: 1960,
+    activo: true,
+    genero: PopRock
+}
+
+const Grupo2: Grupo = {
+    nombre: 'Queen',
+    año: 1970,
+    activo: false,
+    genero: Rock
+}
+
+const Grupo3: Grupo = {
+    nombre: 'AC/DC',
+    año: 1973,
+    activo: true,
+    genero: hardRock
+}
+
+const Grupo4: Grupo = {
+    nombre: 'Beethoven',
+    año: 1970,
+    activo: false,
+    genero: clasica
+}
+
+const Grupo5: Grupo = {
+    nombre: 'The Rolling Stones',
+    año: 1962,
+    activo: true,
+    genero: Rock
+}
+
+console.log(Grupo1)
